8147f6151517a54f46e06b2add843406dcaf0d62
Bruno Balbuena <bbalbuena@pop-os.localdomain>
2021-08-27 03:21:23 -0300

    Selecao das opcoes

--- src/Components/Content/Option.js
+++ src/Components/Content/Option.js
@@ -1,8 +1,14 @@
+import React, { useState } from "react";
+
 const Option = props => {
     const { image, name, description, price } = props;
+    const [selected, setSelected] = useState("menu__option");
 
     return (
-        <li class="menu__option" onclick="">
+        <li
+            class={selected}
+            onClick={() => setSelected("menu__option menu__option--selected")}
+        >
             <img class="menu__image" src={image} />
             <p class="menu__name">{name}</p>
             <p class="menu__description">{description}</p>
